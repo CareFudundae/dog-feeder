0718744b1c71f0c02d5810d7efe4a74594a4c78f
levisonofed <levi.j.elston@gmail.com>
2016-08-08 22:55:48 -0400

    Update and rename camera-test.py to camera-fn.py

---
+++ camera-fn.py
@@ -0,0 +1,37 @@
+from picamera import PiCamera
+from time import sleep
+import time
+from os import system
+
+#mkdir images
+
+startTime = time.time()
+camera = PiCamera()
+
+camera.hflip=True
+camera.vflip=True
+#camera.resolution=(1024,768)
+camera.resolution=(640,480)
+
+#camera warm up
+camera.start_preview()
+sleep(2)
+
+#camera capture
+print('capture start')
+for i in range(10):
+	camera.capture('image{0:04d}.jpg'.format(i))
+	sleep(.5)
+camera.close()
+endTime = time.time()
+print('capture end, time elapsed=',endTime - startTime, 's')
+
+
+#convert jpgs to gif
+print('convert to gif start')
+system('convert -delay 100 -loop 0 image*.jpg animation.gif')
+
+endTime = time.time()
+print('done, total time=', endTime-startTime, 's')
+
+#email image
--- camera-test.py
+++
@@ -1,9 +0,0 @@
-from picamera import PiCamera
-from time import sleep
-
-camera = PiCamera()
-
-for i in range(3)
-	sleep(3)
-	camera.capture('/home/pi/picamtest/image%s.jpg' % i)
-	

e98729e634aa7b50ebc4e10f59206bccd8dffa53
Levi Elston-raspberrypi <levi.j.elston@gmail.com>
2016-08-13 18:58:34 -0400

    updated email blast info

--- blast.py
+++ blast.py
@@ -1,8 +1,19 @@
+import smtplib
 #grab username and password from myinfo.config file
 with open('myinfo.config') as f:
   credentials = [x.strip().split(':') for x in f.readlines()]
+
+#print(credentials)
   
-for usr,pwd in credentials:
-  print(credentials)
-  print(usr)
-  print(pwd)
+for usr,pwd,to_address in credentials:
+#  print(credentials)
+#  print(usr)
+#  print(pwd)
+#  print(to_address)
+  server = smtplib.SMTP('smtp.gmail.com', 587)
+  server.starttls()
+  server.login(usr, pwd)
+ 
+  msg = "test message from pi"
+  server.sendmail(usr, to_address, msg)
+  server.quit()
